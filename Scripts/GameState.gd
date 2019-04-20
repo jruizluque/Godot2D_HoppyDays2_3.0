8298e896474669c5c704494f680ef6e0389f9cd4
Joaquin Ruiz Luque <joaquinruizluque@gmail.com>
2019-04-20 17:50:32 -0300

    Polishing the GUI

--- Scripts/GameState.gd
+++ Scripts/GameState.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
 export var starting_lives = 3
+export var coin_target = 5 #How manu coins for an extra life
+
 var lives
 var coins = 0
+
+onready var GUI = Global.GUI
 
 func _ready():
 	Global.GameState = self
@@ -10,17 +14,31 @@ func _ready():
 	update_GUI()
 
 func update_GUI():
-	Global.GUI.update_GUI(lives)
+	Global.GUI.update_GUI(coins, lives)
+
+func animate_GUI(animation):
+	GUI.animate(animation)
 
 func hurt():
 	lives -= 1
 	update_GUI()
 	Global.Player.hurt()
+	animate_GUI("hurt")
 	if lives < 0:
 		end_game()
 
 func coin_up():
 	coins += 1
+	update_GUI()
+	animate_GUI("CoinPulse")
+	var multiple_of_coin_target = (coins % coin_target) == 0
+	if multiple_of_coin_target:
+		life_up()
+
+func life_up():
+	lives += 1
+	update_GUI()
+	animate_GUI("LifePulse")
 
 func end_game():
 	get_tree().change_scene("res://Scenes/GameOver.tscn")
