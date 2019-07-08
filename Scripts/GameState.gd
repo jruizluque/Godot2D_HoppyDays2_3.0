4b187540e109884e4049f872bbe8a48a0d87186e
Joaquin Ruiz Luque <joaquinruizluque@gmail.com>
2019-07-08 01:03:17 -0300

    Particles and a Portal

--- Scripts/GameState.gd
+++ Scripts/GameState.gd
@@ -43,3 +43,9 @@ func life_up():
 
 func end_game():
 	get_tree().change_scene(Global.GameOver)
+
+func win_game():
+	get_tree().change_scene(Global.Victory)
+
+func _on_Portal_body_entered(body):
+	win_game()
--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -10,4 +10,5 @@ var pain_sfx
 # Scene location - MUST be set MANUALLY
 var Level1 = "res://Scenes/Levels/Level1.tscn"
 var GameOver = "res://Scenes/Levels/GameOver.tscn"
+var Victory = "res://Scenes/Levels/Victory.tscn"
 var Lightning = "res://Scenes/Lightning.tscn"
